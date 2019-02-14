cc9b5083452e2aa8263623b2103ddc4f924020f6
santhoshRaju91 <santhosh.codeholic@gmail.com>
2019-02-14 10:30:41 +0530

    API-mocker: temporary commit

--- mapper.js
+++ mapper.js
@@ -1,14 +1,34 @@
 module.exports = {
-  "items/random": {
+  "risp/search/v1/items/random": {
     method: "GET",
     path: "items_random.json"
   },
-  items: {
+  "risp/search/v1/items": {
     method: "POST",
     path: "items.json"
   },
-  "items/:param1/:param2": {
+  "risp/search/v1/items/:param1/:param2": {
     method: "GET",
     path: "items.json"
+  },
+  "process/image": {
+    method: "POST",
+    path: "process.json"
+  },
+  get_recaptcha: {
+    method: "GET",
+    path: "recaptcha.json"
+  },
+  authenticate: {
+    method: "POST",
+    path: "recaptcha-verify.json"
+  },
+  "risp/search/v1/status": {
+    method: "GET",
+    path: "risp-status.json"
+  },
+  "oauth/token": {
+    method: "POST",
+    path: "risp-login.json"
   }
 };
---
+++ src/mock.js
@@ -0,0 +1,5 @@
+const mockRequests = (req, res) => {
+  const parsedURL = url.parse(req.url, true);
+};
+
+module.exports = mockRequests;
